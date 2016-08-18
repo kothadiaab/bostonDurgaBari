f538c84106ced6d8f98655b0dc3af16e26a3335e
kothadiaab <kothadiaab@gmail.com>
2016-08-17 20:34:41 -0400

    Added routing and header section with styling, Done with Our vision page

--- js/app.js
+++ js/app.js
@@ -1,12 +1,3 @@
 /**
  * Created by abhedkothadia on 8/9/16.
  */
-
-var app = angular.module('bdbApp', ["ngRoute"]);
-
-app.controller("bdbController", bdbControllerFucntion);
-
-function bdbControllerFucntion($scope){
-    $scope.firstName = "Boston";
-    $scope.lastName = "Durga Puja";
-}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
 /**
  * Created by abhedkothadia on 8/9/16.
  */
+
+var bdbApp = angular.module('bdbApp', ['ngRoute']);
+
+//app.controller("bdbController", bdbControllerFucntion);
+//
+//function bdbControllerFucntion($scope){
+//    $scope.firstName = "Boston";
+//    $scope.lastName = "Durga Puja";
+//}
+bdbApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl : "views/home.html"
+        })
+        .when('/our-vision', {
+            templateUrl : "views/our-vision.html",
+            controller  : 'ourVisionController'
+        })
+        .when('/contact-us', {
+            templateUrl : 'views/contact-us.html'
+        })
+        .when('/event-ads', {
+            templateUrl : 'views/event-ads.html'
+        })
+        .when('/our-events', {
+            templateUrl : 'views/our-events.html'
+        })
+});
+
+bdbApp.controller('ourVisionController', function($scope) {
+    // create a message to display in our view
+});
